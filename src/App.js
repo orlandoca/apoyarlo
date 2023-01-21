8e731eb7088eea7e433d4cdac95635087ff0fe5f
orlandoca <orlandoca647@gmail.com>
2023-01-20 22:50:41 -0300

    commit de apoyarlo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PersonCard from "./components/Personcard.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containt-main">
+        <PersonCard first="Doe " last="Jane" age="45" hair="Black" />
+        <PersonCard first="Smith" last="John" age="88" hair="Brown" />
+        <PersonCard first="Fillmore" last="Millard" age="50" hair="Brown" />
+        <PersonCard first="Smith" last="Maria" age="62" hair="Brown" />
+      </div>
     </div>
   );
 }
